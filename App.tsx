b37abc00ee296a8ae1b04369c8fe5389f6200791
asst211118 <asst211118@tosspartners.com>
2022-04-03 00:07:29 +0900

    feat: :sparkles: webview 연결

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-// import {WebView} from 'react-native-webview';
-import MainView from './src/screens/MainView';
+import * as React from 'react';
+import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-const App = () => {
-  return <MainView />;
-};
+function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#61sfd" />
+      <WebView source={{uri: 'https://sulsul-fe.vercel.app/'}} />
+    </SafeAreaView>
+  );
+}
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
